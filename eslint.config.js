2c8028805f604b2435774f6f367ff4460330c046
minhnhc <nhcminh47@gmail.com>
2025-05-20 12:16:36 +0700

    create config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,68 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  js.configs.recommended,
+  prettier,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        React: 'writable',
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+      react: 'plugin:react/recommended',
+      '@typescript-eslint': '@typescript-eslint/eslint-plugin',
+      'simple-import-sort': 'plugin:simple-import-sort/recommended',
+      'import-helpers': 'plugin:import-helpers/recommended',
+      'unused-imports': 'plugin:unused-imports',
+      'jsx-a11y': 'plugin:jsx-a11y/recommended',
+      'import': 'plugin:import/recommended',
+      'react-hooks': 'plugin:react-hooks/recommended',
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'prettier/prettier': 'error',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            ['^@/'],
+            ['^\\u0000'],
+            ['^.+\\.s?css$'],
+          ],
+        },
       ],
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          groups: [
+            ['/^react/', '/^@/'],
+            ['/^components/', '/^hooks/', '/^services/', '/^utils/'],
+            ['/^styles/'],
+            ['/^types/'],
+          ],
+        },
+      ],
+      'unused-imports/no-unused-imports-ts': 'error',
+      'jsx-a11y/no-onchange': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
-)
+];
